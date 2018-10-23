03115e85b01a86a8462dc20ac111ceff79fd640e
Dolany <dolany@sina.cn>
2018-10-23 23:45:38 +0800

    xx

---
+++ TestWebApp/AopInterceptor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Castle.DynamicProxy;
+using System.Diagnostics;
+using NLog;
+
+namespace TestWebApp
+{
+    public abstract class AopInterceptor : IInterceptor
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        public void Intercept(IInvocation invocation)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+
+            var methodName = $"{invocation.TargetType.FullName}.{invocation.Method.Name}";
+
+            Logger.Info($"aop :{methodName} --->");
+
+            Exception temp_ex = null;
+            try
+            {
+                invocation.Proceed();
+            }
+            catch (Exception ex)
+            {
+                temp_ex = ex;
+                Logger.Error(ex);
+            }
+            sw.Stop();
+            var elapsedMilliseconds = sw.ElapsedMilliseconds;
+            Logger.Fatal($"aop :{methodName} <---耗时{elapsedMilliseconds}毫秒");
+
+            if (temp_ex != null)
+                throw temp_ex;
+        }
+    }
+}
--- TestWebApp/Startup.cs
+++ TestWebApp/Startup.cs
@@ -1,14 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Autofac.Extras.DynamicProxy;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using NLog.Web;
 
 namespace TestWebApp
 {
@@ -22,7 +27,7 @@ namespace TestWebApp
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
@@ -31,13 +36,32 @@ namespace TestWebApp
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
+            services.Replace(ServiceDescriptor.Transient<IControllerActivator, ServiceBasedControllerActivator>());
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            var assembly = typeof(Startup).Assembly;
+
+            var builder = new ContainerBuilder();
+            builder.Populate(services);
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces();
+
+            var IControllerType = typeof(ControllerBase);
+            builder.RegisterAssemblyTypes(assembly).Where(t =>
+                IControllerType.IsAssignableFrom(t) && t != IControllerType).PropertiesAutowired();
+
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces().EnableInterfaceInterceptors()
+                .InterceptedBy(typeof(AopInterceptor));
+
+            var Container = builder.Build();
+            return new AutofacServiceProvider(Container);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddNLog();//添加NLog
+            env.ConfigureNLog("nlog.config");//读取Nlog配置文件
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
